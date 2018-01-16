433c7a5860c35ea0f80126cbd32b8e0d039a0d3e
Gil Sunshine <gilsunshine@gmail.com>
2018-01-16 19:24:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, attribute_type|
+    attribute_type.each do |type, names|
+      names.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name].has_key?(attribute)
+          pigeon_list[name][attribute] = []
+        end
+        if names.include?(name)
+          pigeon_list[name][attribute].push(type.to_s)
+        end
+      end
+    end
+  end
+  pigeon_list
 end
